69e1187d2d788f403ba06a1e4987d08f02ecb03e
Loek Janssen <ljanssen@stanford.edu>
2015-12-15 15:20:52 -0800

    Raw Scores

--- data/total/parse_scores.py
+++ data/total/parse_scores.py
@@ -28,13 +28,6 @@ class Students(object):
 		else:
 			self.student_dict[sunet] = [grade]
 
-	def setDropDict(self):
-		for sunet in self.student_dict.keys():
-			# only include hws
-			score_list = self.student_dict[sunet][:6]
-
-			# drop idx num
-			self.student_drop[sunet] = self.student_dict[sunet].index(str(min([int(score) for score in score_list])))
 
 	def checkExists(self, sunet):
 		return bool(sunet in self.student_dict)
@@ -46,58 +39,7 @@ class Students(object):
 				return False
 		return True
 
-	def addTotal(self):
-		self.headers.append('total')
-		for sunet, grade_lst in self.student_dict.iteritems():
-			sum_scores = 0
-			for idx, score in enumerate(grade_lst):
-				# skip over minimum idx
-				if idx == self.student_drop[sunet]:
-					continue
-				sum_scores += int(score)
-			self.student_dict[sunet].append(str(sum_scores))
 	
-	def addNormalized(self):
-		# get total scores list per TA, but drop outliers and quiz scores in calculation
-		TA_scores = {}
-		for sunet, scores in self.student_dict.iteritems():
-			TA = self.map_TA[sunet]
-			if TA not in TA_scores:
-				# -1 is total score minus minimum, 6 and 7 is quizzes
-				TA_scores[TA] = [int(scores[-1]) - int(scores[6]) - int(scores[7])]
-			else:
-				TA_scores[TA].append(int(scores[-1]) - int(scores[6]) - int(scores[7]))
-
-		# get mean total and mean per TA
-		tot_mean = 0.0
-		TA_mean = {}
-		for TA, scores in TA_scores.iteritems():
-
-			# get rid of outliers
-			sort_scores = sorted(scores)
-			Q1 = sort_scores[int(len(scores) * (1.0/4))]
-			Q3 = sort_scores[int(len(scores) * (3.0/4))]
-			IQR = Q3 - Q1
-			no_out = [score for score in sort_scores if score > (Q1 - 1.5*IQR) and score < (Q3 + 1.5*IQR)]
-			
-			mean = utils.mean(no_out)
-			tot_mean += mean
-			TA_mean[TA] = mean
-		
-		# mean everyone should adjust to (means together, divided by num TA's)
-		avg_mean = tot_mean / float(len(TA_mean))
-
-		TA_adjust = {}
-		print("\nAdjust score by:")
-		for TA in TA_mean.keys():
-			TA_adjust[TA] = avg_mean - TA_mean[TA] 
-			print("TA:\t\t{0}\nadjust score:\t{1}".format(TA, float(TA_adjust[TA])))
-		
-		
-		self.headers.append('normalized')
-		for sunet, grade_lst in self.student_dict.iteritems():
-			self.student_dict[sunet].append(str(int(round(float(self.student_dict[sunet][-1]) + TA_adjust[self.map_TA[sunet]]))))
-			
 
 	def addTA(self):
 		self.headers.append('TA')
@@ -106,8 +48,6 @@ class Students(object):
 
 	def toCSV(self):
 
-		self.addTotal()
-		self.addNormalized()
 		self.addTA()
 
 		self.headers.insert(0,'sunet')
@@ -115,7 +55,7 @@ class Students(object):
 		# adding empty cols to fill in excel/pages/googlesheets
 		self.headers += ['empty1','empty2','empty3']
 
-		with open('gradebook.csv', 'w') as f:
+		with open('raw_scores.csv', 'w') as f:
 			# write headers
 			f.write(','.join(self.headers))
 			f.write('\n')
@@ -276,10 +216,6 @@ def main():
 	if not students.checkConsistent():
 		raise RuntimeError('Dictionary is not consistent. Different number of grades per student...')
 
-	# Decide which hw to drop
-	students.setDropDict()
-
-	# Calc total, normalized etc.. and write to CSV
 	students.toCSV()
 
 if __name__=='__main__':
