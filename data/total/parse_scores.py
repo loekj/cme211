532800793967b029beab9fcefb1abd835ea80fa8
loekj <ljanssen@stanford.edu>
2015-12-15 15:07:47 -0800

    data sclaed

--- data/total/parse_scores.py
+++ data/total/parse_scores.py
@@ -4,15 +4,12 @@ import utils
 HWFILES = ['hw1','hw2','hw3','hw4','hw5','hw6']
 QUIZFILES = ['quiz1','quiz2']
 PROJECTFILES = ['project1','project2']
-# one hw is dropped, so divided by 5
-WEIGHTS = [0.5/5,0.125/2,0.25/2] #hws, quizes, project
 
 class Students(object):
 
 	def __init__(self, headers, map_TA):
 		self.headers = headers#+['total']+['normalized']+['TA']
 		self.student_dict = {}
-		self.student_drop = {}
 
 		with open(map_TA) as f:
 			map_TA_list = f.readlines()
@@ -30,13 +27,28 @@ class Students(object):
 		else:
 			self.student_dict[sunet] = [grade]
 
-	def setDropDict(self):
+	def dropMin(self)
 		for sunet in self.student_dict.keys():
 			# only include hws
 			score_list = self.student_dict[sunet][:6]
 
 			# drop idx num
-			self.student_drop[sunet] = self.student_dict[sunet].index(min(score_list))
+			self.student_dict[sunet].pop(self.student_dict[sunet].index(min(score_list)))
+
+	def getHWscore(self, weight):
+		for sunet in self.student_dict.keys():
+			rem_scores = self.student_dict[sunet][5:]
+			self.student_dict[sunet] = [weight*sum(self.student_dict[sunet][:5])] + rem_scores
+
+	def getQuizScore(self, weight):
+		for sunet in self.student_dict.keys():
+			self.student_dict[sunet][1] = weight*(self.student_dict[sunet][1] + self.student_dict[sunet][2])
+			self.student_dict[sunet].pop(2)
+
+	def getProjectScore(self, weight):
+		for sunet in self.student_dict.keys():
+			self.student_dict[sunet][2] = weight*(self.student_dict[sunet][2]+self.student_dict[sunet][3])
+			self.student_dict[sunet].pop(3)
 
 	def checkExists(self, sunet):
 		return bool(sunet in self.student_dict)
@@ -51,13 +63,7 @@ class Students(object):
 	def addTotal(self):
 		self.headers.append('total')
 		for sunet, grade_lst in self.student_dict.iteritems():
-			sum_scores = 0
-			for idx, score in enumerate(grade_lst):
-				# skip over minimum idx
-				if idx == self.student_drop[sunet]:
-					continue
-				sum_scores += score
-			self.student_dict[sunet].append(sum_scores)
+			self.student_dict[sunet].append(sum(grade_lst))
 	
 	def addNormalized(self):
 		# get total scores list per TA, but drop outliers and quiz scores in calculation
@@ -65,10 +71,10 @@ class Students(object):
 		for sunet, scores in self.student_dict.iteritems():
 			TA = self.map_TA[sunet]
 			if TA not in TA_scores:
-				# -1 is total score minus minimum, 6 and 7 is quizzes
-				TA_scores[TA] = [scores[-1] - scores[6] - scores[7]]
+				# -1 is total score minus minimum, quiz is idx 1
+				TA_scores[TA] = [scores[-1] - scores[1]]
 			else:
-				TA_scores[TA].append(scores[-1] - scores[6] - scores[7])
+				TA_scores[TA].append(scores[-1] - scores[1])
 
 		# get mean total and mean per TA
 		tot_mean = 0.0
@@ -98,13 +104,13 @@ class Students(object):
 		
 		self.headers.append('normalized')
 		for sunet, grade_lst in self.student_dict.iteritems():
-			self.student_dict[sunet].append(int(round(self.student_dict[sunet][-1] + TA_adjust[self.map_TA[sunet]])))
+			self.student_dict[sunet].append(self.student_dict[sunet][-1] + TA_adjust[self.map_TA[sunet]])
 			
 
 	def addTA(self):
 		self.headers.append('TA')
 		for sunet, TA in self.map_TA.iteritems():
-			self.student_dict[sunet].append(TA)		
+			self.student_dict[sunet].append(TA)	
 
 	def toCSV(self):
 
@@ -155,7 +161,7 @@ def main():
 		raise RuntimeError(map_ta + 'does not exist!')
 
 	# Init object from class, headers are in order of processing
-	students = Students(HWFILES+QUIZFILES+PROJECTFILES, map_TA)
+	students = Students(['HWs', 'QUIZs', 'PROJECT'])
 
 	# Process HWs
 	for num_hw, hw in enumerate(hw_paths):
@@ -177,7 +183,7 @@ def main():
 				if num_hw != 0:
 					if not students.checkExists(sunet):
 						raise RuntimeError('Student '+sunet+' does not exist in checking hw '+str(num_hw+1))
-				students.addGrade(sunet, score, WEIGHTS[0])
+				students.addGrade(sunet, score)
 
 
 	# Process QUIZs
@@ -204,7 +210,7 @@ def main():
 			sunet, score = line_lst[1].lower(), line_lst[9]
 			if not students.checkExists(sunet):
 				raise RuntimeError('Student '+sunet+' does not exist in quiz1?')
-			students.addGrade(sunet, score, WEIGHTS[1])
+			students.addGrade(studentset, score)
 
 	with open(quiz2, 'r') as f:
 		print('Burning line "'+f.readline()+'"')
@@ -229,7 +235,7 @@ def main():
 			print(score)
 			if not students.checkExists(sunet):
 				raise RuntimeError('Student '+sunet+' does not exist in quiz2?')
-			students.addGrade(sunet, score, WEIGHTS[1])
+			students.addGrade(sunet, score)
 
 	# Process Project
 	project1, project2 = project_paths[0], project_paths[1]
@@ -251,7 +257,7 @@ def main():
 			if num_hw != 0:
 				if not students.checkExists(sunet):
 					raise RuntimeError('Student '+sunet+' does not exist in project1?')
-			students.addGrade(sunet, score, WEIGHTS[2])
+			students.addGrade(sunet, score)
 
 	with open(project2, 'r') as f:
 		for line in f:
@@ -271,14 +277,21 @@ def main():
 			if num_hw != 0:
 				if not students.checkExists(sunet):
 					raise RuntimeError('Student '+sunet+' does not exist in project2?')
-			students.addGrade(sunet, score, WEIGHTS[2])
+			students.addGrade(sunet, score)
 
 
 	if not students.checkConsistent():
 		raise RuntimeError('Dictionary is not consistent. Different number of grades per student...')
 
-	# Decide which hw to drop
-	students.setDropDict()
+	# drop min
+	students.dropMin()
+
+	# Concat hw scores into one score times 0.5
+	students.getHWscore(0.5)
+
+	students.getQuizScore(0.25)
+
+	students.getProjectScore(0.25)
 
 	# Calc total, normalized etc.. and write to CSV
 	students.toCSV()
